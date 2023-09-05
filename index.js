230d1b4a70f61ca93ab9b1d51cb83ba2ecd12712
melissageoghegan <melissageoghegan95@gmail.com>
2023-09-05 17:26:51 +0100

    created node app

---
+++ index.js
@@ -0,0 +1,18 @@
+const express=require("express");
+const app= express();
+app.use(express.static(__dirname+"/Public"));
+app.listen(3000, function() {
+    console.log('Server running on port 3000');
+});
+app.get("/", function(req, res) {
+    res.sendFile(__dirname+"/Public/TGSWebsite-Products.html");
+});
+app.get("/Tutorials", function(req, res) {
+    res.sendFile(__dirname+"/Public/TGSWebsite-Tutorials.html");
+});
+app.get("/BookLessons", function(req, res) {
+    res.sendFile(__dirname+"/Public/TGSWebsite-BookLessons.html");
+});
+app.get("/AboutMe", function(req, res) {
+    res.sendFile(__dirname+"/Public/TGSWebsite-AboutMe.html");
+});
